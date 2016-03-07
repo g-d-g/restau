3717a54de5371493d7574407eeca272b6e8a9348
Steven Enten <steven@enten.fr>
2016-03-07 17:15:48 +0100

    Bump v0.3.0

--- index.js
+++ index.js
@@ -13,34 +13,3 @@ module.exports = Object.assign(restau, {
   Service,
   errors
 });
-
-// TODO
-// api public .client(headers) pour retourner une api permettant de
-// d'exécuter des requêtes http vers les services existants
-//
-// api public .remote(String, headers) pour retourner un middleware interceptant
-// les routes des services et exécutant la requête vers le service
-// distant avec le client
-//
-// ModelService
-// static get idField = id
-// static get tableName = name
-// static get schema
-// static get disable
-//
-// TODO gérer l'authentification
-//
-// TODO "client" ou proxy vers un service distant
-// à ce moment là toutes les routes du registre
-// pointent sur un service externe
-// + paramètres de config de screws
-//    { remote: { url, headers }}
-// TODO le paramètre change la création du handlerFlow
-//   en mode "remote" on transfert la requête vers
-//   un service distant et affiche le résultat en brut
-//
-// TODO utiliser endgame
-// endgame(options.uncaughtException);
-//
-// TODO lorsque l'on inject les services dans app.services,
-// il faut créer app.services[endpoint][get|post|patch] ???
